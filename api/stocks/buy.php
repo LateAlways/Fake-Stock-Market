4af32a6906a4a7504fc5c23bda4fb177c0085223
LateAlways <110630690+LateAlways@users.noreply.github.com>
2022-11-21 20:32:32 -0500

    Update buy.php

--- api/stocks/buy.php
+++ api/stocks/buy.php
@@ -15,11 +15,11 @@ foreach($accounts as $acc) {
         try {
             $acc_data = json_decode(file_get_contents("../../data/userdata/".((string)$acc["id"]).".json"), true);
             $stock_data = json_decode(file_get_contents("https://query1.finance.yahoo.com/v8/finance/chart/".strtoupper($symbol)."?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance"), true);
-            $acc_data["balance"] -= $stock_data["chart"]["result"][0]["meta"]["regularMarketPrice"];
             for ($x = 0; $x <= (int) $amount; $x++) {
+                $acc_data["balance"] -= $stock_data["chart"]["result"][0]["meta"]["regularMarketPrice"];
                 $acc_data["actions"][] = array("id" => $acc_data["action_increment"]+1, "symbol" => $symbol, "price_bought" => $stock_data["chart"]["result"][0]["meta"]["regularMarketPrice"]);
+                $acc_data["action_increment"] += 1;
             }
-            $acc_data["action_increment"] += 1;
             file_put_contents("../../data/userdata/".((string)$acc["id"]).".json", json_encode($acc_data));
             echo json_encode(array("success" => true, "action_id" => $acc_data["actions"][count($acc_data["actions"])-1]["id"]));
             exit;
